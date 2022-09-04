39b88f4354386bf882e2830f5e4d670c8b022e30
zhangyulongcau1992 <zhangyulong_cau@163.com>
2022-09-04 17:27:19 +0800

    添加SQL

---
+++ Hibernate/src/sql/test.sql
@@ -0,0 +1,116 @@
+/*
+SQLyog v10.2 
+MySQL - 5.5.62 : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `course` */
+
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `course` */
+
+insert  into `course`(`id`,`name`) values (2,'asasd'),(4,'数学');
+
+/*Table structure for table `course_student` */
+
+DROP TABLE IF EXISTS `course_student`;
+
+CREATE TABLE `course_student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sid` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKes4tje2qb5ydu4tdoaew9kevs` (`sid`),
+  KEY `FK7wbvfppy9es5c6fns3269buau` (`cid`),
+  CONSTRAINT `FK7wbvfppy9es5c6fns3269buau` FOREIGN KEY (`cid`) REFERENCES `course` (`id`),
+  CONSTRAINT `FKes4tje2qb5ydu4tdoaew9kevs` FOREIGN KEY (`sid`) REFERENCES `student` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Data for the table `course_student` */
+
+insert  into `course_student`(`id`,`sid`,`cid`) values (3,3,2),(5,5,4);
+
+/*Table structure for table `customer` */
+
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `customer` */
+
+insert  into `customer`(`id`,`name`) values (1,'张三');
+
+/*Table structure for table `orders` */
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) DEFAULT NULL,
+  `cid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `orders_fk` (`cid`),
+  CONSTRAINT `orders_fk` FOREIGN KEY (`cid`) REFERENCES `customer` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `orders` */
+
+insert  into `orders`(`id`,`name`,`cid`) values (1,'订单1',1),(2,'订单2',1),(3,'订单3',1),(4,'订单1',1);
+
+/*Table structure for table `people` */
+
+DROP TABLE IF EXISTS `people`;
+
+CREATE TABLE `people` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) DEFAULT NULL,
+  `money` double DEFAULT NULL,
+  `nation` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+/*Data for the table `people` */
+
+insert  into `people`(`id`,`name`,`money`,`nation`) values (1,'李四664',101011,NULL),(2,'陆雪琪664',200,'China'),(3,'李四',200,'China'),(4,NULL,300,NULL),(5,NULL,400,NULL),(6,'李四五',200,'China'),(7,'王健林',200,'China'),(8,'王健林',200,'China'),(9,'张强',200,'China'),(10,'树先生',200,'China'),(11,'大马哈',200,'China'),(12,'大马哈',200,'China'),(13,'abcdefg',100,'China');
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Data for the table `student` */
+
+insert  into `student`(`id`,`name`) values (3,'王五664'),(5,'张三');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
